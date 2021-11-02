c17384c44e974b63db58359c680ffa19c5bbccbf
Matthew Hefferon <matthefferon@gmail.com>
2021-11-01 20:16:25 -0600

    We have a chart people

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import "./App.css";
+import React from "react";
+import ReactEcharts from "echarts-for-react";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <ReactEcharts
+        option={{
+          xAxis: {
+            type: "category",
+            data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+          },
+          yAxis: {
+            type: "value",
+          },
+          series: [
+            {
+              data: [820, 932, 901, 934, 1290, 1330, 1320],
+              type: "line",
+            },
+          ],
+        }}
+      />
     </div>
   );
 }
